cd6ba3cc46fe7d064737715fc9f3a3ce2abe1b11
Abdelwahab <abdelwahabeladawy1611@gmail.com>
2025-02-20 16:54:58 +0200

    the page of staff aaugmentation get translated

--- src/component/12-StaffAugmentation/StaffAugmentation.jsx
+++ src/component/12-StaffAugmentation/StaffAugmentation.jsx
@@ -1,135 +1,198 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import QualityAssurance from "../../assets/images/OFFf.svg";
 import QualityAssurance3 from "../../assets/images/computer.svg";
+import { t } from "i18next";
+import { useTranslation } from "react-i18next";
 
 export default function StaffAugmentation() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  const { t, i18n } = useTranslation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    return (
-     
-            <div className="">
-                <div className="">
-                    <div className="mt-16 mb-14 flex justify-center items-center text-xl">
-                        <img src={QualityAssurance} alt="QualityAssurance2" />
-                        <p className="ps-2 font-montserrat text-[#003540] dark:text-[#B3E5F1] text-hover font-bold text-xl tracking-[1.6px]">
-                            Services
-                        </p>
-                    </div>
-        
-                    <div className="flex justify-center items-center">
-                        <h2 className="text-4xl font-montserrat text-[#003540] dark:text-[#B3E5F1] font-bold mb-[80px] text-center tracking-[4.8px] leading-[75px] w-full sm:w-auto">
-                            Scale your team with top-tier talent, tailored to your project needs.
-                        </h2>
-                    </div>
-        
-                    <div className="lg:px-[121px] px-14 flex-col justify-center items-center">
-                        <h1 className="text-center text-[#003540] dark:text-[#B3E5F1] mb-[20px] text-primary font-montserrat text-[32px] tracking-[3.2px] font-[500] w-full sm:w-auto">
-                            Staff Augmentation
-                        </h1>
-                        <p className="text-aboutCont text-xl text-center text-[#666] dark:text-white font-montserrat font-normal leading-[30px] tracking-[1.6px] mb-[60px] w-full sm:w-auto">
-                            Staff augmentation is a versatile solution that enables organizations
-                            to boost agility and quickly adapt to evolving business demands. This
-                            approach is widely utilized across industries to fulfill critical
-                            roles in Engineering, Product Development, and Operations.
-                        </p>
-                    </div>
-        
-                    <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Identify: Specialized Skills
-                            </h4>
-                            <p className="font-montserrat text-[16px] text-xl text-[#666] dark:text-white font-normal tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Share the Job description for the open roles, hire one employee or
-                                100 for a month or a lifetime
-                            </p>
-                        </div>
-        
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                    </div>
-        
-                    <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                        <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Access to larger talent pool
-                            </h4>
-                            <p className="font-montserrat text-xl text-[#666] dark:text-white font-normal tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Provide shortlisted candidates from its talents pool with 10,000+
-                                candidates
-                            </p>
-                        </div>
-                    </div>
-        
-                    <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Interview
-                            </h4>
-                            <p className="font-montserrat text-xl font-normal text-[#666] dark:text-white tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Run your own interview if needed for the shortlisted candidates
-                                (optional step)
-                            </p>
-                        </div>
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                    </div>
-        
-                    <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                        <div className="w-full sm:w-3/4 pt-14 flex-col-reverse justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Hire
-                            </h4>
-                            <p className="font-montserrat text-xl text-[#666] dark:text-white font-normal tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Sign statement of work (SOW) and start working, flexible terms
-                                that match your business, and suitable for your use case
-                            </p>
-                        </div>
-                    </div>
-        
-                    <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Onboard
-                            </h4>
-                            <p className="font-montserrat text-xl font-normal text-[#666] dark:text-white tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Our seamless onboard process ensures your employees join your team
-                                and get up to speed quickly employers and employees enjoy fully
-                                self-serve onboarding and management tools
-                            </p>
-                        </div>
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                    </div>
-        
-                    <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
-                        <div className="w-full sm:w-1/4">
-                            <img src={QualityAssurance3} className="w-full rounded-l-full" alt="" />
-                        </div>
-                        <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
-                            <h4 className="font-montserrat text-[20px] font-[600] tracking-[2px] mb-4 dark:text-[#B3E5F1] w-full">
-                                Invoice & pay
-                            </h4>
-                            <p className="font-montserrat text-xl font-normal text-[#666] dark:text-white tracking-[1.6px] leading-[30px] w-full sm:w-auto">
-                                Handle payroll, tax, benefits, and compliance for your globally
-                                distributed team here, with no hidden fees
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
-        
-    );
+  return (
+    <div className="">
+      <div className="">
+        <div className="mt-16 sm:mb-14 mb-6 flex justify-center items-center text-xl ">
+          <img src={QualityAssurance} alt="" className="w-8 h-8" />
+          <p
+            className={` ps-2 font-montserrat font-bold text-lg tracking-wide text-[#295F70] dark:text-[#B3E5F1]  ${
+              i18n.language === "ar" && "font-cairo text-[33px]"
+            } `}
+          >
+            {t("services")}
+          </p>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <h2
+            className={`text-sm sm:text-3xl font-mons font-bold sm:mb-[80px] mb-[50px] text-center ${
+              i18n.language === "ar" && "font-cairo "
+            } text-[#003540]  leading-[75px] dark:text-[#B3E5F1] ${
+              i18n.language === "en" && "tracking-[2.8px]"
+            } `}
+          >
+            {t("staffHeading")}{" "}
+          </h2>
+        </div>
+
+        <div className="lg:px-[121px] px-14 flex-col justify-center items-center">
+          <h1
+            className={`text-center text-[#003540] dark:text-[#B3E5F1] mb-[20px] text-primary font-montserrat text-[32px]  font-[500] w-full sm:w-auto ${
+              i18n.language === "ar" && "font-cairo"
+            }   ${i18n.language === "en" && "tracking-[3.2px]"}`}
+          >
+            {t("staff")}
+          </h1>
+          <p
+            className={`text-aboutCont text-xl text-center text-[#666] dark:text-white font-montserrat  leading-[30px] ] mb-[60px] w-full sm:w-auto  ${
+              i18n.language === "ar" && "font-cairo"
+            }   ${i18n.language === "en" && "tracking-[1.6px]"}`}
+          >
+            {t("staffDesc")}
+          </p>
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
+            <h4
+              className={`font-montserrat text-[20px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[2px]"}`}
+            >
+              {t("specialzed-skills")}
+            </h4>
+            <p
+              className={`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}
+            >
+              {t("specialzedDesc")}
+            </p>
+          </div>
+
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+          <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
+            <h4
+              className={`font-montserrat text-[18px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[2px]"}`}
+            >
+             
+              {t("access")}
+            </h4>
+            <p className= {`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}>
+              {t("accessDesc")}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
+            <h4
+              className={`font-montserrat text-[20px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[2px]"}`}
+            >
+       
+              {t("interview")}
+            </h4>
+            <p  className={`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}>
+              {t("interviewDesc")}
+            </p>
+          </div>
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+          <div className="w-full sm:w-3/4 pt-14 flex-col-reverse justify-center items-center ps-1 mt-[-30px]">
+            <h4 className={`font-montserrat text-[20px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${i18n.language==='ar'&& "font-cairo"}   ${i18n.language==='en'&& "tracking-[2px]"}`}>
+            
+              {t("hire")}
+            </h4>
+            <p className={`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}>
+              {t("hireDesc")}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
+            <h4 className={`font-montserrat text-[20px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${i18n.language==='ar'&& "font-cairo"}   ${i18n.language==='en'&& "tracking-[2px]"}`}>
+            
+              {t("onboard")}
+            </h4>
+            <p className={`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}>
+              {t("onboardDesc")}
+            </p>
+          </div>
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center lg:px-[121px] px-14 mb-16 gap-16">
+          <div className="w-full sm:w-1/4">
+            <img
+              src={QualityAssurance3}
+              className="w-full rounded-l-full"
+              alt=""
+            />
+          </div>
+          <div className="w-full sm:w-3/4 pt-14 flex-col justify-center items-center ps-1 mt-[-30px]">
+            <h4 className={`font-montserrat text-[20px] font-[600] text-center lg:text-start mb-4 dark:text-[#B3E5F1] w-full ${i18n.language==='ar'&& "font-cairo"}   ${i18n.language==='en'&& "tracking-[2px]"}`}>
+            
+              {t("invoice")}
+            </h4>
+            <p className={`font-montserrat text-[22px] text-xl text-[#666] dark:text-white  text-center lg:text-start  leading-[30px] w-full sm:w-auto ${
+                i18n.language === "ar" && "font-cairo"
+              }   ${i18n.language === "en" && "tracking-[1.6px]"}`}>
+              {t("invoiceDesc")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
